b6d2ce8fb5c996eacfc9cd2d8c8cc6f1104da9e1
Adis Sharma <adissharma@Mac.broadband>
2026-09-13 17:55:01 +0100

    Reduce dashboard countdown text size

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1043,8 +1043,8 @@ const styles = StyleSheet.create({
   upcomingHeroCountdown: {
     color: '#FFFFFF',
     fontFamily: fontFamilies.textMedium,
-    fontSize: 17,
-    lineHeight: 21,
+    fontSize: 15,
+    lineHeight: 19,
     letterSpacing: 0,
   },
   completedCarouselContent: {
